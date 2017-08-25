4fdea6e4113af77ded1342c28bae90b4303f76f7
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-08-25 09:59:19 +0300

    end datastore.js

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -3,10 +3,30 @@
   var App = window.App || {};
 
   function DataStore() {
-    console.log('Hello');
     this.data = {};
   }
-
+  DataStore.prototype.add = function (key, val) {
+    this.data[key] = val;
+  }
+  DataStore.prototype.get = function (key) {
+    return this.data[key];
+  }
+  DataStore.prototype.getAll = function () {
+    return this.data;
+  }
+  DataStore.prototype.remove = function (key) {
+    delete this.data[key];
+  }
   App.DataStore = DataStore;
   window.App = App;
 })(window);
+
+/*var dsOne = new App.DataStore();
+var dsTwo = new App.DataStore();
+dsOne.data['email'] = 'james@bond.com';
+dsOne.data['order'] = 'black coffee';
+dsTwo.data['email'] = 'moneypenny@bond.com';
+dsTwo.data['order'] = 'Chai tea';
+dsOne.data;
+dsTwo.data;
+*/
